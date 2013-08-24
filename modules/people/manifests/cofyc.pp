9b89d1a0d082bcaa9c5f5499be26a487c316fd6d
Yecheng Fu <cofyc.jackson@gmail.com>
2013-08-24 18:38:08 +0800

    tools

--- modules/people/manifests/cofyc.pp
+++ modules/people/manifests/cofyc.pp
@@ -1,7 +1,19 @@
 class people::cofyc {
   package {
     [
-      "coreutils"
+      "coreutils",
+      "ascii",
+      "bash",
+      "bash-completion",
+      "cmake",
+      "ctags",
+      "curl",
+      "gnu-indent",
+      "gnu-sed",
+      "gnu-tar",
+      "irssi",
+      "mercurial",
+      "p7zip",
     ]:
   }
 }
